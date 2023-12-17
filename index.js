fc3a08972b5ca91cf2b616bcaf0d6f7fbe64ccbc
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-12-17 09:46:46 +0100

    made some minor adjustments

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const email = document.querySelector('#email');
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    email.validity.typeMismatch
-        ? (error.textContent = 'Please provide a valid email address')
-        : email.value === ''
-        ? (error.textContent =
-              'Whoops! It looks like you forgot to add your email')
-        : (error.textContent = '');
+    if (email.validity.typeMismatch) {
+        error.style.display = 'block';
+        error.textContent = 'Please provide a valid email address';
+        email.classList.add('invalid');
+    } else if (email.value === '') {
+        error.style.display = 'block';
+        error.textContent =
+            'Whoops! It looks like you forgot to add your email';
+        email.classList.add('invalid');
+    } else {
+        error.style.display = 'none';
+        error.textContent = '';
+        email.classList.remove('invalid');
+    }
 });
